c5c97df3d78acc89500427ecbeb237ed5c49c466
Georgios Vervelakis <verevlakis@vbankcenter.ru>
2025-05-15 16:33:17 +0300

    исправил тест заказа добавил фикстуру

--- test/test_create_order.py
+++ test/test_create_order.py
@@ -12,24 +12,11 @@ class TestCreateOrder:
         ["BLACK",
         "GREY",],
         []
-
     ])
     @allure.title('можно создать заказ с такими цветами — {colour_list}')
-    def test_create_order_color(self,colour_list):
+    def test_create_order_color(self,colour_list,test_create_order_dto):
 
-        payload = {
-            "firstName": "Naruto",
-            "lastName": "Uchiha",
-            "address": "Konoha, 142 apt.",
-            "metroStation": 4,
-            "phone": "+7 800 355 35 35",
-            "rentTime": 5,
-            "deliveryDate": "2020-06-06",
-            "comment": "Saske, come back to Konoha",
-            "color": colour_list
-        }
-
-        response = requests.post(f"{Urls.base_url}{Urls.api_create_order}", data=json.dumps(payload))
+        response = requests.post(f"{Urls.base_url}{Urls.api_create_order}", data=json.dumps(test_create_order_dto))
 
         assert response.status_code == 201
         assert 'track' in response.json()
