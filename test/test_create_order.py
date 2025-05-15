f21d8c640b89ad546766fbf389380addcfc23e2e
Georgios Vervelakis <verevlakis@vbankcenter.ru>
2025-05-15 15:49:33 +0300

    исправил тест
    исправил тест , сделал параметризацию

--- test/test_create_order.py
+++ test/test_create_order.py
@@ -6,12 +6,16 @@ import allure
 
 class TestCreateOrder:
 
-    @pytest.mark.parametrize("colour", [
-        "BLACK",
-        "GREY",
+    @pytest.mark.parametrize("colour_list", [
+        ["BLACK"],
+        ["GREY"],
+        ["BLACK",
+        "GREY",],
+        []
+
     ])
-    @allure.title('можно указать один из цветов — BLACK или GREY')
-    def test_create_order_one_color(self,colour):
+    @allure.title('можно создать заказ с такими цветами — {colour_list}')
+    def test_create_order_color(self,colour_list):
 
         payload = {
             "firstName": "Naruto",
@@ -22,49 +26,7 @@ class TestCreateOrder:
             "rentTime": 5,
             "deliveryDate": "2020-06-06",
             "comment": "Saske, come back to Konoha",
-            "color": [colour]
-        }
-
-        response = requests.post(f"{Urls.base_url}{Urls.api_create_order}", data=json.dumps(payload))
-
-        assert response.status_code == 201
-        assert 'track' in response.json()
-
-
-    @allure.title('можно указать оба цвета')
-    def test_create_order_twice_color(self):
-
-        payload = {
-            "firstName": "Naruto",
-            "lastName": "Uchiha",
-            "address": "Konoha, 142 apt.",
-            "metroStation": 4,
-            "phone": "+7 800 355 35 35",
-            "rentTime": 5,
-            "deliveryDate": "2020-06-06",
-            "comment": "Saske, come back to Konoha",
-            "color": ["BLACK","GREY"]
-        }
-
-        response = requests.post(f"{Urls.base_url}{Urls.api_create_order}", data=json.dumps(payload))
-
-        assert response.status_code == 201
-        assert 'track' in response.json()
-
-
-    @allure.title('можно совсем не указывать цвет')
-    def test_create_order_no_color(self):
-
-        payload = {
-            "firstName": "Naruto",
-            "lastName": "Uchiha",
-            "address": "Konoha, 142 apt.",
-            "metroStation": 4,
-            "phone": "+7 800 355 35 35",
-            "rentTime": 5,
-            "deliveryDate": "2020-06-06",
-            "comment": "Saske, come back to Konoha",
-            "color": []
+            "color": colour_list
         }
 
         response = requests.post(f"{Urls.base_url}{Urls.api_create_order}", data=json.dumps(payload))
