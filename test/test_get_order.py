cac9740b1b2758cfa9453f5d8c6c224b68275b3b
Georgios Vervelakis <verevlakis@vbankcenter.ru>
2025-05-15 16:53:49 +0300

    добавил тест на получение списка заказов

---
+++ test/test_get_order.py
@@ -0,0 +1,13 @@
+import requests
+from test_data import Urls
+import allure
+
+class TestGetOrder:
+
+    @allure.title('Проверка, что в тело ответа возвращается список заказов')
+    def test_get_order(self):
+
+            response = requests.get(f"{Urls.base_url}{Urls.api_get_order}")
+
+            assert response.status_code == 200
+            assert 'orders' in response.json()
--- test_data.py
+++ test_data.py
@@ -1,6 +1,8 @@
 class Urls:
 
     base_url = "https://qa-scooter.praktikum-services.ru"
+
     api_create_courier = "/api/v1/courier"
     api_login_courier = "/api/v1/courier/login"
     api_create_order = "/api/v1/orders"
+    api_get_order = "/api/v1/orders"
